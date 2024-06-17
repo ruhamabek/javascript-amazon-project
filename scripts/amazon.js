d05bf3da748703288fd4903cecae07565f2212a9
ruhamabek <ruhamauzubo@gmail.com>
2024-06-17 11:57:41 +0300

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,14 @@ document.querySelector('.js-products-grid')
               quantity : 1
        });
        }
-         console.log(cart);
+       
+       let cartQuantity = 0;
+
+       cart.forEach((item)=>{
+               cartQuantity += item.quantity;
+       });
+       
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
            });
     });
